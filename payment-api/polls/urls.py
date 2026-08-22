fc0cfe3d50e6e5b07bfdda9275ea4d1c15c6bec9
Rodrigo Carvalho <rodrigocarvalhoshen@gmail.com>
2026-08-22 11:32:04 -0300

    finishing PART 3 Django framework python and changing the README.md archive

--- payment-api/polls/urls.py
+++ payment-api/polls/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
     path("", views.index, name="index"),
     path("contact/", views.contact, name="contact"),
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/results", views.results, name="results"),
+    path("<int:question_id>/votes", views.vote, name="vote")
 ]
--- payment-api/polls/views.py
+++ payment-api/polls/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Question
+from django.http import Http404
 
 def index( request ):
-    return HttpResponse("hello, World. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list" : latest_question_list}
+    return render(request, "polls/index.html", context)
 
 def contact( request ):
     return HttpResponse("You're in the Contact page of the polls.")
+
+def detail(request, question_id):
+    # try :
+    question = get_object_or_404( Question, pk=question_id)
+    # except:
+    #     raise Http404("Question does not exist")
+    return render(request, "polls/details.html", {"question" : question})
+
+def results(request, question_id):
+    response = "You're looking at the results of the questions: %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're looking at the votes of the questions: %s." % question_id)
