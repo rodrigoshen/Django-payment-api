620490dd76d26b0fe9c7fe3d77e479a1188c113b
Rodrigo Carvalho <rodrigocarvalhoshen@gmail.com>
2026-09-01 12:13:01 -0300

    PART 4 finishing

--- payment-api/polls/urls.py
+++ payment-api/polls/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("contact/", views.contact, name="contact"),
     path("<int:question_id>/", views.detail, name="detail"),
     path("<int:question_id>/results", views.results, name="results"),
-    path("<int:question_id>/votes", views.vote, name="vote")
+    path("<int:question_id>/votes", views.vote, name="vote"),
+    path("<int:question_id>/vote/", views.vote, name="vote")
 ]
